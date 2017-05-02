aa168e8dd0120087a0269ae32adfd725cf8f698f
rdruhland <ryan.ruhland@cdw.com>
2017-05-02 13:38:54 -0500

    x->z and square 24

--- src/my_square.py
+++ src/my_square.py
@@ -1,8 +1,8 @@
-def my_square(x):
+def my_square(z):
 	"""Takes a value and returns the squared value
 	
-	uses the ** operator
+	uses the * operator
 	"""
-	return (x**2)
+	return (z*z)
 	
-print(my_square(4))
+print(my_square(24))
